e07ee88c66dcd98be2edfc19ad090cc07739f15e
Kristiyan Dzhakov <kristiyandzhakov@Kristiyans-MacBook-Pro.local>
2019-01-30 06:56:26 +0000

    Initial commit - webpack dev server up and running, react implemented

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+export default class App extends Component {
+    render(){
+        return (
+            <div className="main">
+                <h1>Hello World!</h1>
+            </div>
+            
+        );
+    };
+};
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require('path');
+
+
+module.exports = {
+    mode: 'development',
+    entry: ['./src/index.js'],
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8080
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js|.jsx$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["react"]
+                }
+                
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    
+};
